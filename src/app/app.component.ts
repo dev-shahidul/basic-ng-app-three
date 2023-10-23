0fb22724fb03dffdda21619795f9e512ee43f23d
Md. Shahid-ul Islam <shahid@Mds-MacBook-Pro.local>
2023-10-23 08:50:40 +0600

    simple app done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'basic-app-three';
+  password = 'Secret Password = tuna';
+  showPassword = false;
+  log: number[] = [];
+
+  onToggleDetails() {
+    this.showPassword = !this.showPassword;
+    this.log.push(this.log.length + 1);
+  }
 }
